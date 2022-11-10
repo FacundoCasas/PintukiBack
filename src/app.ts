8cd9763a34c37d510a4a3d5c8f2651e2380f7151
FacundoCasas <75185801+FacundoCasas@users.noreply.github.com>
2022-11-09 23:10:06 -0300

    Hello World

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
